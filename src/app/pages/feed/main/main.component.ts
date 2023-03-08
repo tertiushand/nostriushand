79af0fd8c47781ac2dd53485f21dc348736d9f44
tertiushand <tertiushand@gmail.com>
2023-03-07 20:31:58 -0700

    beginning to pull data for main feed.

--- src/app/pages/feed/main/main.component.ts
+++ src/app/pages/feed/main/main.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TopicHead } from 'src/app/shared/components/topic-head/topic-head.interface';
+import { StorageHelperService } from 'src/app/shared/services/storage-helper.service';
+import { EventIds, NostrMsgHelperService } from 'src/app/shared/services/websocket/nostr-msg-helper.service';
+import { iNipEvent } from 'src/app/shared/services/websocket/nostr.interface';
+import { RelayService } from 'src/app/shared/services/websocket/relay.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  constructor(
+    private nostrMsg: NostrMsgHelperService,
+    private relays: RelayService,
+    private storage: StorageHelperService
+  ){}
+
+  ngOnInit(): void {
+    this.populateFeedNotes();
+  }
+
+  private relayUrl: string = this.relays.relays[0];
+
+  populateFeedNotes() {
+
+    this.nostrMsg.initiateNewRelay(this.relayUrl);
+    console.log('listening to '+this.relayUrl);
+
+    if (this.storage.getUserInfo().pubkey) {
+      this.relays.initializedRelays[this.relayUrl].sendMessage({
+        requestId: 'INITIAL_USER_COLLECT',
+        authors: [this.storage.getUserInfo().pubkey as string],
+        kinds: [0,1,2,3]
+      });
+    }
+  }
 
   public topics: TopicHead[] = [
     { name: 'Following', route: ['feed'] },
